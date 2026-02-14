d71170dc205b6b47fc7e7f05f2142c71eb3f5fff
Shorunmu Ayodeji <111268999+Shorunmu-Ayodeji@users.noreply.github.com>
2026-02-13 17:13:53 -0800

    chore: silence eslint no-unused-vars warnings in a few files

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import PasscodeGate from "./components/PasscodeGate";
 import StorySection from "./components/StorySection";
